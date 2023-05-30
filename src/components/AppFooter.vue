d551cecbaf8a07ed3e55ba825cd3020c6ea092b9
Paolo96700 <paolo.falcoapp@gmail.com>
2023-05-30 17:23:01 +0200

    sviluppo footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,27 +5,46 @@ export default {
 </script>
 
 <template>
-
-    <div class="container" style="color: white">
-        <div>
-            <img src="./../assets/chess/images/mt-2236-home-logo.png" alt="">
-            <p style="font-size: 11px;">Copyright 2021, All right Riserved.</p> 
+<section class="section_footer">
+    <div class="container d-flex" style="color: white">
+        <div class="footer_left d-flex flex-column justify-content-between">
+            <div style="width: 70%">
+                <img src="./../assets/chess/images/mt-2236-home-logo.png" alt="" class="img-fluid">
+            </div>
+            <div>
+                <p style="font-size: 11px;">Copyright 2021, All right Riserved.</p> 
+            </div>
         </div>
-        <div>
-
+        
+        <div class="footer_central">
+            <p>Follow Us</p>
+            <div>
+                <i class="fa-brands fa-facebook-f"></i>
+            </div> 
         </div>
         <div>
 
         </div>
     </div>
+</section>
+   
   
 </template>
 
 
 <style lang="scss" scoped>
-    .container{
-        width: 80%;
-        height: 15rem;
+
+    .section_footer{
         background-color: rgb(28, 28, 28);
+    }
+    .container{
+        width: 65%;
+        height: 15rem;
+        
+    }
+
+    .footer_left{
+        height: 100%;
+        padding-top: 3.2rem ;
     }
 </style>
